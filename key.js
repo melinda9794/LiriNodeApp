32decf3618614f0192a8fcbe5e96e38dea0568f6
Melinda <melinda9794@gmail.com>
2017-02-20 15:23:28 -0500

    made changes to twitter

--- key.js
+++ key.js
@@ -1,4 +1,4 @@
-exports.twitterKeys = {
+module.exports = {
   consumer_key: 'dySjMJhqyPu8RXLMggvoYrCYf',
   consumer_secret: 'dxgt6tQUbi8aXC1WZguAKhTGzy96Q8bX7Nr7WvGaiKi7K1zi4X',
   access_token_key: '610898165-P3wyYS2DYAZkZb1qOoa0Yq9hRpn6gfWHMWOY0PR5',
--- liri.js
+++ liri.js
@@ -1,37 +1,35 @@
 var fs = require("fs");
-var keys = require("./keys.js");
+var twitterKeys = require("./key.js");
 var twitter = require("twitter");
-var params = process.argv.slice(2);
+var params = process.argv[2];
+var value = process.argv[3];
 
 //gets the twitter api from keys.js
-function myTweets() {
+//function myTweets() {
   var client = new twitter({
-    consumer_key: keys.twitterKeys.consumer_key,
-    consumer_secret: keys.twitterKeys.consumer_secret,
-    access_token_key:  keys.twitterKeys.access_token_key,
-    access_token_secret: keys.twitterKeys.access_token_secret   
+    consumer_key: twitterKeys.consumer_key,
+    consumer_secret: twitterKeys.consumer_secret,
+    access_token_key:  twitterKeys.access_token_key,
+    access_token_secret: twitterKeys.access_token_secret   
   });
 
-
-  switch(params[0]){
-    case "my-tweets":
-    case "tweets":
-      myTweets();
-  }
+console.log(client);
+  
 
 //grabs the last 20 tweets of the user from the twitter api and displays and logs them
-function myTweets(){
-  client.get('statuses/user_timeline', function(error, tweets, response){
-    console.log(error);
-    if(error) {
-      console.log(error);
-      throw error;
-    }
-    console.log("Your last 20 tweets:")
-    for (i = 0; i < 20; i++){
-      console.log(i+1 + ". On " + tweets[i].created_at + " You Tweeted: " + tweets[i].text);
-      fs.appendFile("log.txt", i+1 + ". On " + tweets[i].created_at + " You Tweeted: " + tweets[i].text + "\n", function(err){
-      })
-    }
-  });
-}
+// function myTweets(){
+//   client.get('statuses/user_timeline', function(error, tweets, response){
+//     console.log(error);
+//     if(error) {
+//       console.log(error);
+//       throw error;
+//     }
+//     console.log("Your last 20 tweets:")
+//     for (i = 0; i < 20; i++){
+//       console.log(i+1 + ". On " + tweets[i].created_at + " You Tweeted: " + tweets[i].text);
+//       fs.appendFile("log.txt", i+1 + ". On " + tweets[i].created_at + " You Tweeted: " + tweets[i].text + "\n", function(err){
+//       })
+//     }
+//   });
+// }
+// myTweets();
